23bc6b6af5ca2d551a7666d13a74ed94e1deabf9
easydoesit <michael.grills@gmail.com>
2024-11-26 19:39:00 -0700

    name change

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
-import express from 'express';
+import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
+import bodyParser from 'body-parser';
 
 dotenv.config({ path: path.join(process.cwd(), "./.env") });
 
-const app = express();
-const port = process.env.PORT;
+const app:Express = express();
 
-app.get('/', (req, res) => {
+const port = process.env.PORT;
+const jsonParser = bodyParser.json();
+
+app.get('/', jsonParser, (req:Request, res:Response) => {
   res.send('Express + Typescript Server');
+
 })
 
 app.listen(port, () => {
